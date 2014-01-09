7af39010773fd7760adb8d0aa76ed5ead1f4b202
Daniel Ma <danielma@bubargain.com>
2014-01-09 14:28:13 +0800

    add email send function

--- inquiry.php
+++ inquiry.php
@@ -1,3 +1,32 @@
+<?php 
+$errors = '';
+$myemail = 'contact@kitetea.com';
+if(empty($_POST['name'])  || 
+   empty($_POST['email']) || 
+   empty($_POST['message']))
+{
+    $errors .= "\n Error: Please fill in all the required fields";
+}
+
+$name = $_POST['name']; 
+$email_address = $_POST['email']; 
+$message = $_POST['message']; 
+
+if( empty($errors))
+{
+	$to = $myemail; 
+	$email_subject = "KITE Inquiry";
+	$email_body = "
+	Name: $name
+	Email: $email_address
+	Message:
+	$message"; 
+	
+	$headers = "From: $email_address";
+	
+	mail($to,$email_subject,$email_body,$headers);
+} 
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -6,7 +35,9 @@
 </head>
 <body>
 
-<br />
- Error: Please fill in all the required fields
+<?php
+echo nl2br($errors);
+?>
+
 </body>
 </html>
--- newsletter.php
+++ newsletter.php
@@ -1,3 +1,28 @@
+<?php 
+$errors = '';
+$myemail = 'contact@kitetea.com';
+if(empty($_POST['name'])  || 
+   empty($_POST['email']))
+{
+    $errors .= "\n Error: Please fill in all the required fields";
+}
+
+$name = $_POST['name']; 
+$email_address = $_POST['email']; 
+
+if( empty($errors))
+{
+	$to = $myemail; 
+	$email_subject = "KITE Subscription";
+	$email_body = "
+	Name: $name
+	Email: $email_address";
+	
+	$headers = "From: $email_address";
+	
+	mail($to,$email_subject,$email_body,$headers);
+} 
+?>
 <!doctype html>
 <html lang="en">
 <head>
@@ -6,7 +31,9 @@
 </head>
 <body>
 
-<br />
- Error: Please fill in all the required fields
+<?php
+echo nl2br($errors);
+?>
+
 </body>
 </html>
