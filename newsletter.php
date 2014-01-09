6983560b3170724734e69a38ef4bad907a8d1aea
Daniel Ma <danielma@bubargain.com>
2014-01-09 10:26:37 +0800

    upload new index page

---
+++ newsletter.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<title>Invalid Form</title>
+</head>
+<body>
+
+<br />
+ Error: Please fill in all the required fields
+</body>
+</html>
